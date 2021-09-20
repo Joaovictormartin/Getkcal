325dbdc674a931e1ca2a8f56d6a9c78d714b3824
Joao <joao.teixeira@aedb.br>
2021-09-20 00:03:04 -0300

    criando e ajustando o Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import { Container } from './styles';
+import { Container, Content, WraperLabel, Label } from "./styles";
 
 export function Home() {
-  return(
+  const [sex, setSex] = useState("Masculino");
+  const [age, setAge] = useState("");
+  const [Weight, setWeight] = useState("");
+  const [Height, setHeight] = useState("");
+  const [activityLevel, setactivityLevel] = useState("");
+
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+
+  },[])
+
+  return (
     <Container>
-      
+      <Content>
+
+        <WraperLabel>
+          <Label>Seu sexo</Label>
+        </WraperLabel>
+        <select 
+          id="sex" 
+          value={sex} 
+          onChange={(e) => setSex(e.target.value)}
+        >
+          <option value=""></option>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+        <WraperLabel>
+          <Label>Sua idade</Label>
+        </WraperLabel>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <WraperLabel>
+          <Label>Seu peso</Label>          
+        </WraperLabel>
+        <input
+          type="text"
+          value={Weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+
+        <WraperLabel>
+          <Label>Sua altura</Label> 
+        </WraperLabel>
+        <input
+          type="text"
+          value={Height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+
+        <WraperLabel>
+          <Label>Seu nível de atividade fisíca</Label>
+        </WraperLabel>
+        <select
+          id="activityLevel"
+          value={activityLevel}
+          onChange={(e) => setactivityLevel(e.target.value)}
+        >
+          <option value=""></option>
+          <option value="1.2">Sedentário</option>
+          <option value="1.375">Pouca atividade</option>
+          <option value="1.55">Atividade moderada</option>
+          <option value="1.725">Atividade intensa</option>
+          <option value="1.9">Atividade muito intensa</option>
+        </select>
+
+        <button type="submit" onClick={handleSubmit}>Calcular</button>
+      </Content>
     </Container>
   );
 }
-
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,65 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  max-width: 1120px;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+
+export const Content = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  input, select {
+    width: 80%;
+    height: 3rem;
+    padding: 0 1rem;
+    font-size: 1rem;
+
+    background: var(--background-input);
+    color: var(--text-color);
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+
+    margin-bottom: 2rem;
+
+
+    &:focus {
+      border-color: var(--border-input);
+    }
+
+  }
+
+  button {
+    background-image: linear-gradient(-90deg, #FF7373 0%, #FDDB33 100%);
+    width: 80%;
+    height: 3rem;
+
+    border: 0;
+    border-radius: 0.25rem;
+    font-size: 1.2rem;
+    font-weight: 600;
+
+    cursor: pointer;
+  }
+`;
+
+export const WraperLabel = styled.div`
+  width: 80%;
+  margin-bottom: 5px;
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+export const Label = styled.p`
+  font-size: 1rem;
+`;
+
