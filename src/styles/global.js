3f903f4036b075df3c5baca8a654e0875d166330
Joao <joao.teixeira@aedb.br>
2021-09-19 22:25:57 -0300

    Criando o styles global

---
+++ src/styles/global.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle  } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #282828;
+    --text-color: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+  }
+
+  body {
+    background: var(--background);
+    font-family: 'Montserrat', sans-serif;
+    color: var(--text-color);
+  }
+
+  html {
+    @media(max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media(max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body, input, textarea, button {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+`;
