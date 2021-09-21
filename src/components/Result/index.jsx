5d5a6ecdb08946f18f1995be1863e53fc16ceeb1
Joao <joao.teixeira@aedb.br>
2021-09-20 22:14:05 -0300

    Projeto finalizado

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -3,17 +3,18 @@ import React from "react";
 import { Container, Title, Content } from "./styles";
 
 export function Result({ gender, age, weight, height, activityLevel }) {
-  
   const tmb = Math.round(
-    gender === 'female'
-      ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
-      : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
+    gender === "female"
+      ? 655 + 9.6 * weight + 1.8 * height - 4.7 * age
+      : 66 + 13.7 * weight + 5 * height - 6.8 * age
   );
   const maintenance = Math.round(tmb * Number(activityLevel));
   const loseWeight = maintenance - 450;
   const gainWeight = maintenance + 450;
+  const height100 = height/100;
+  const imc = weight / (height100*height100);
 
-  console.log(tmb)
+  console.log(tmb);
 
   return (
     <Container>
@@ -21,6 +22,10 @@ export function Result({ gender, age, weight, height, activityLevel }) {
 
       <Content>
         <ul>
+          <li>
+            Seu IMC - (Índice de Massa Corporal) foi de{" "}
+            <strong>{imc ? imc.toFixed(2) : 0}</strong>.
+          </li>
           <li>
             Seu metabolismo basal é de <strong>${tmb && tmb} calorias</strong>.
           </li>
